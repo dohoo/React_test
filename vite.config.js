2360ceace07186bcc828b9e994b89d125d693a83
dohoo <dohoo2007@naver.com>
2025-07-24 01:36:53 +0900

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/React_test/", // <-- 반드시 본인 저장소 이름
   plugins: [react()],
 })
